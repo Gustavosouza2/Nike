6d8d1d8cb08c853fa60c94e5994757a6ec85bd57
Gustavosouza2 <gustavoleonsouza@gmail.com>
2023-06-06 20:04:56 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,92 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import Logo from "../images/logo.png";
+import Boot from "../images/air force cano alto.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="bg-white">
+      <header className="block ">
+        <nav className='font-sans border-gray-200 px-2 lg:px-2 py-2.5 bg-white "'>
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl mt-20 gap-42">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
+              src={Logo}
+              alt="logo da nike"
               width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+              height={52}
+              className="cursor-pointer transform hover:rotate-180 transition duration-500 ease-in-out 
+              text-white font-bold rounded-lg z-30"
+            ></Image>
+            <li
+              className="list-none text-bgcolor text-4xl transform 
+                                transition duration-500 hover:scale-125 cursor-pointer z-30 "
+            >
+              View
+            </li>
+            <li
+              className="list-none text-bgcolor text-3xl  transform 
+                                transition duration-500 hover:scale-125 cursor-pointer  z-30 "
+            >
+              Sale
+            </li>
+            <li
+              className="list-none text-bgcolor text-3xl  transform 
+                                transition duration-500 hover:scale-125 cursor-pointer z-30"
+            >
+              Men
+            </li>
+            <li
+              className="list-none text-bgcolor text-3xl  transform 
+                                transition duration-500 hover:scale-125 cursor-pointer  z-30 "
+            >
+              Woman
+            </li>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+            <button
+              className="rounded-full ring-4 ring-gray-800 ring-inset px-12 py-4 text-2xl
+            transform 
+            transition duration-500 hover:scale-110 
+            cursor-pointer z-30 shadow-2xl"
+            >
+              Search
+            </button>
+          </div>
+        </nav>
+      </header>
+
+      <div className="">
+        <h2
+          className="text-pink-200  text-5xl absolute opacity-0.1 z-0"
+          style={{ writingMode: "vertical-rl" }}
+        >
+          <div className="flex justify-center items-center ml-64">
+            <div className="w-full relative">
+              <div className="bg-pink-300  mix-blend-multiply animation-delay-2000 animate-blob absolute filter blur-3xl inset-0 m-auto -bottom-8 h-128 w-128 rounded-full"></div>
+            </div>
+          </div>
+          AIR
+        </h2>
+      </div>
+      <div className="flex justify-center items-center pt-16">
+        <h1 className="text-pink-100 text-5xl text-right pl-paddingleft opacity-0.1 z-10 font-sans-light">
+          <div className="flex z-20 ml-24 mt-32 ">
+            <div className="w-full relative  ">
+              <div className="bg-pink-300  mix-blend-multiply absolute animation-delay-2000 animate-blob filter blur-3xl inset-0 m-auto -bottom-8 h-128 w-128 rounded-full "></div>
+            </div>
+          </div>
+          <p className="pl-16 text-justify">JUST</p>
+          DO <br />
+          IT.
+        </h1>
+
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          src={Boot}
+          alt="nike air force"
+          width={986}
+          height={456}
+          className="z-10 absolute"
+        ></Image>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div></div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,66 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "140px",
+    },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-futura)"],
+        roboto: ["var(--font-roboto)"],
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(20px, -20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+      },
+      padding: {
+        paddingleft: "45rem",
+      },
+      margin: {
+        marginright: "60rem",
+      },
+      rotate: {
+        rotatedeg: "270deg",
+      },
+      height: {
+        128: "40rem",
+      },
+      width: {
+        128: "40rem",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
